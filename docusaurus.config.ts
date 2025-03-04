56b0eab26365e677712fbbeea0b709226f2aa3b7
JinalTandel <jinal.tandel@1rivet.com>
2025-03-04 16:00:11 +0530

    added admin files

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,30 +2,20 @@ import type * as Preset from "@docusaurus/preset-classic";
 import type { Config } from "@docusaurus/types";
 import { themes as prismThemes } from "prism-react-renderer";
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: "Aekatra",
   tagline: "Aekatra is a website to manage data of an IT company.",
   favicon: "img/favicon.svg",
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: "http://localhost:3000",
   baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "facebook",
+  projectName: "docusaurus",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -39,14 +29,6 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           routeBasePath: "docs",
           include: ["**/*.md", "**/*.mdx"],
-          // includeCurrentVersion: false,
-          // lastVersion: "current",
-          // versions: {
-          //   current: {
-          //     label: "1.0.1",
-          //     path: "1.0.1",
-          //   },
-          // },
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
@@ -56,14 +38,8 @@ const config: Config = {
             type: ["rss", "atom"],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
         },
         theme: {
           customCss: "./src/css/custom.css",
@@ -102,6 +78,13 @@ const config: Config = {
           position: "right",
         },
       ],
+    },
+    algolia: {
+      apiKey: "88c72c7e4ffe0bdc1533709f2f3b2c9a",
+      appId: "2PV46KESUL",
+      indexName: "personalData",
+      contextualSearch: true,
+      placeholder: "Search Data",
     },
     footer: {
       style: "dark",
